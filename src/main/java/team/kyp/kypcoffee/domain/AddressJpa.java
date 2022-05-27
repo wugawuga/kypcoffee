d069a9b5067867154f0358c795d07b6e905a536d
Wugawuga <dnr3655@naver.com>
2022-05-27 21:46:10 +0900

    add MemberEntity

--- src/main/java/team/kyp/kypcoffee/domain/AddressJpa.java
+++ src/main/java/team/kyp/kypcoffee/domain/AddressJpa.java
@@ -1,2 +1,19 @@
-package team.kyp.kypcoffee.domain;public class AddressJpa {
+package team.kyp.kypcoffee.domain;
+
+import javax.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class AddressJpa {
+
+    private String ad;
+
+    protected AddressJpa() {
+
+    }
+
+    public AddressJpa(String ad) {
+        this.ad = ad;
+    }
 }
--- src/main/java/team/kyp/kypcoffee/domain/MemberJpa.java
+++ src/main/java/team/kyp/kypcoffee/domain/MemberJpa.java
@@ -1,2 +1,36 @@
-package team.kyp.kypcoffee.domain;public class MemberJpa {
+package team.kyp.kypcoffee.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class MemberJpa {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "memberNum")
+    private int memberNum;
+
+    private String memberId;
+    private String memberPw;
+    private String memberName;
+    private Date memberBday;
+
+    @Embedded
+    private AddressJpa addressJpa;
+
+    private String memberTel;
+    private String memberPhone;
+    private String memberEmail;
+    private int memberEmailYn;
+    private int memberMileage;
+    private int memberType;
 }
