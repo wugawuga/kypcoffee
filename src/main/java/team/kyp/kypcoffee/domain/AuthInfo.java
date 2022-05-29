8f5959d2d8eedd90dcf815b5ef957a2d88801b37
Wugawuga <dnr3655@naver.com>
2022-05-29 15:17:10 +0900

    refactor - AuthInfo 수정

--- src/main/java/team/kyp/kypcoffee/domain/AuthInfo.java
+++ src/main/java/team/kyp/kypcoffee/domain/AuthInfo.java
@@ -1,8 +1,10 @@
 package team.kyp.kypcoffee.domain;
 
 import lombok.Data;
+import lombok.Getter;
 
 @Data
+@Getter
 public class AuthInfo { //세션에 로그인 정보 저장
 
     private String id;
@@ -13,97 +15,15 @@ public class AuthInfo { //세션에 로그인 정보 저장
     private String picture;
     private int type;
 
-    public AuthInfo () {}
-
-    public AuthInfo(String id, String name, int no, String pw, int type) {
+    private AuthInfo(String id, String name, String email, int type) {
         this.id = id;
         this.name = name;
-        this.no = no;
-        this.pw = pw;
+        this.email = email;
         this.type = type;
     }
 
-    public AuthInfo(String id, String name, int no, String pw) {
-        this.id = id;
-        this.name = name;
-        this.no = no;
-        this.pw = pw;
+    public static AuthInfo sessionCreate(String id, String name, String email, int type) {
+
+        return new AuthInfo(id, name, email, type);
     }
-
-    public AuthInfo(String id, String name, int no) {
-        this.id = id;
-        this.name = name;
-        this.no = no;
-    }
-
-    public AuthInfo(String id, String name, int no, String pw, String email, String picture) {
-        this.id = id;
-        this.name = name;
-        this.no = no;
-        this.pw = pw;
-        this.email = email;
-        this.picture = picture;
-    }
-
-    public AuthInfo(String id, String name, int no, String pw, String email, String picture,int type) {
-        this.id = id;
-        this.name = name;
-        this.no = no;
-        this.pw = pw;
-        this.email = email;
-        this.picture = picture;
-        this.type = type;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNo() {
-        return no;
-    }
-
-    public void setNo(int no) {
-        this.no = no;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    public int getType() { return type; }
-
-    public void setType(int type) { this.type = type; }
 }
