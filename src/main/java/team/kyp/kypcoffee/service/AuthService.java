f706fe74913e740afc817063c5095e469347565c
Wugawuga <dnr3655@naver.com>
2022-05-29 15:18:48 +0900

    refactor - AuthService 수정

--- src/main/java/team/kyp/kypcoffee/service/AuthService.java
+++ src/main/java/team/kyp/kypcoffee/service/AuthService.java
@@ -3,31 +3,37 @@ package team.kyp.kypcoffee.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.kyp.kypcoffee.domain.AuthInfo;
+import team.kyp.kypcoffee.domain.LoginCommand;
 import team.kyp.kypcoffee.domain.Member;
+import team.kyp.kypcoffee.domain.MemberJpa;
 import team.kyp.kypcoffee.exception.IdPasswordNotMatchingException;
 import team.kyp.kypcoffee.mapper.MemberMapper;
+import team.kyp.kypcoffee.repository.MemberRepository;
 
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuthService {
 
-    @Autowired
-    MemberMapper mapper;
+    private final MemberRepository memberRepository;
+    public AuthInfo authenticate(LoginCommand loginCommand) {
 
-    public AuthInfo authenticate(String id, String name, int no, String pw) {
+        MemberJpa memberJpa = memberRepository.findByMemberid(loginCommand.getId());
 
-        Member member = mapper.selectByIdAll(id);
-
-        if(member==null) {
+        if(memberJpa==null) {
             throw new IdPasswordNotMatchingException();
         }
-        if(!member.getMemberPw().equals(pw)) {
+        if(!memberJpa.getMemberPw().equals(loginCommand.getPw())) {
             throw new IdPasswordNotMatchingException();
         }
 
-        return new AuthInfo(member.getMemberId(), member.getMemberName(), member.getMemberNum(), member.getMemberPw(),member.getMemberType());
+        return AuthInfo.sessionCreate(memberJpa.getMemberId(),
+            memberJpa.getMemberName(),
+            memberJpa.getMemberEmail(),
+            memberJpa.getMemberInfoJpa().getMemberType());
     }
 
 }
